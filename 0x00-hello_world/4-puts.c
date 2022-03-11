7579fc722e84c858e40af7a2aa5b64d77fdd071d
Damisarasaq <damisarasaq@outlook.com>
2022-03-10 17:10:38 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+
+/**
+
+ *main - entry point
+
+ *
+
+ *Return: 0 after printing the fubction
+
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
