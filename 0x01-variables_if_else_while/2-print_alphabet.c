dd1a72979f1dd7ceea4aed95fbe5b5e320d4df5f
Damisarasaq <damisarasaq@outlook.com>
2022-03-14 16:31:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *prints the alphabet in lowercase
+ *return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while(char c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar(\n);
+return(0);
+}
