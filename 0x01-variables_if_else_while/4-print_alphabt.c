6fda10474679bca22779b03c8b234a51173bc7dc
Damisarasaq <damisarasaq@outlook.com>
2022-03-14 17:49:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-Print all the letters except q and e
+ *You can only use the putchar function
+ *Return:0
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != q && ch != e)
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}
