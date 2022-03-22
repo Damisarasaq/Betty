2d4aeaefa0e0034bf93938d782ea383da4b4a524
Damisarasaq <damisarasaq@outlook.com>
2022-03-22 16:03:02 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@ int count = 0;
 while (count >= 0)
 {
 if (str[count] == '\0')
-{	  
+{
 _putchar('\n');
 break;
-}      
-if (count % 2 == 0)	
+}
+if (count % 2 == 0)
 _putchar(str[count]);
 count++;
-}  
+}
 }
