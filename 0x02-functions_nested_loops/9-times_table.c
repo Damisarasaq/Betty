8a0b03d03987e6f382614b3c40ad3df13c812d9b
Damisarasaq <damisarasaq@outlook.com>
2022-03-28 18:11:37 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,35 @@ void times_table(void)
 {
 int i, j, n, x, y;
 for (i = 0; i < 10; ++i)
-{      
+{
 for (j = 0; j < 10; ++j)
-{	  
-n = i * j;	  
-if (j == 0)	    
-{	      
-_putchar('0');	      
-}	  
-else if (n > 9)	    
-{	      
+{
+n = i * j;
+if (j == 0)
+{
+_putchar('0');
+}
+else if (n > 9)
+{
 x = n / 10 + '0';
 y = n % 10 + '0';
-_putchar(x);	      
-_putchar(y);	      
-}	  
-else	    
-{	      
-_putchar(' ');	      
-_putchar(n + '0');	      
-}	  
-if (j < 9)	    
-{
-_putchar(',');	      
-_putchar(' '); 
+_putchar(x);
+_putchar(y);
 }
-else   
+else
 {
-_putchar('\n'); 
-} 
+_putchar(' ');
+_putchar(n + '0');
+}
+if (j < 9)  
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n');
+}
 }
 } 
 }
