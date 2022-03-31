b8496c936a9ccd3d6e5bd3a9d742574b103e83f8
Damisarasaq <damisarasaq@outlook.com>
2022-03-31 15:59:25 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,12 +9,12 @@
  * Return: spot that matches or NULL
  */
 char *test(char *s5, char *s6, char *t5, char *t6)
-{  
+{
 if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
 return (t5);
 else if (*s5 != *s6)
 return (wildcard(s5, t6));
-else   
+else
 return (test(s5 + 1, s6 + 1, t5, t6));
 }
 /**
@@ -25,9 +25,9 @@ return (test(s5 + 1, s6 + 1, t5, t6));
  * Return: pointer to spot in s3 that matches s4
  */
 char *wildcard(char *s3, char *s4)
-{  
+{
 if (*s3 != *s4)
-{      
+{
 if (*s3 == '\0')
 return (0);
 return (wildcard(s3 + 1, s4));
