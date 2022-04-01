a0ea650a0f9aed34cc0acf5b32657f7e6e1d8235
Damisarasaq <damisarasaq@outlook.com>
2022-03-31 17:41:19 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,10 +13,10 @@ int main(void)
 int i, flag;
 unsigned long n1, n2, t, rem, fp_n2, sp_n2, fp_n1, sp_n1, t1, t2;
 n1 = 0;
-n2 = 1; 
+n2 = 1;
 flag = 0;
 for (i = 0; i < 98; ++i)
-{     
+{
 if (n2 < 1000000000000000000)
 {
 t =  n1 + n2;
