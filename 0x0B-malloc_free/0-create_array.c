bb66d8af7211cb5b452962918ac393036c19f602
Damisarasaq <damisarasaq@outlook.com>
2022-04-05 15:31:02 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,17 +7,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *p; 
+char *p;
 unsigned int i;
-if (size > 0)    
+if (size > 0)
 {
 p = malloc(size * sizeof(char));
 if (p == NULL)
-return (NULL); 
+return (NULL);
 }
 else
 return (NULL);
 for (i = 0; i < size; i++)
 p[i] = c;
-return (p); 
+return (p);
 }
